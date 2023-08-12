663b40082795416b2b185ba99a4a050709a3a291
chaad98 <faritzchad@yahoo.com>
2023-08-12 21:09:49 +0800

    First push project

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,50 @@
+class BooksController < ApplicationController
+    before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @books = Book.all
+    end
+
+    def show
+        @book = Book.find(params[:id])
+    end
+
+    def new
+        @book = Book.new
+    end
+
+    def create
+        @book = Book.new(book_params)
+        if @book.save
+            redirect_to root_path, notice: 'Book created successfully!'
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @book.update(book_params)
+            redirect_to root_path, notice: 'Book updated successfully!'
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @book.destroy
+        redirect_to root_path, notice: 'Book deleted successfully!'
+    end
+
+    private
+
+    def book_params
+        params.require(:book).permit(:author, :title)
+    end
+
+    def set_book
+        @book = Book.find(params[:id])
+    end
+end
